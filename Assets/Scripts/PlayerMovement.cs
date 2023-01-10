36884168a56dc61b237b1e53d834738aaced1564
Mansur Besleney <besleneymansur@gmail.com>
2023-01-10 17:11:10 +0300

    PlayerMovement Script Added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float playerSpeed = 1f;
+    [SerializeField] private float playerHorizontalSpeed = 10f;
+    /*
+    [SerializeField] private Rigidbody playerRB;
+    */
+    [SerializeField] private GameObject Player;
+    
+    [SerializeField] private CharacterController controller;
+    private Vector3 direction;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Player = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         direction.z = playerSpeed;
+         direction.x = Input.GetAxisRaw("Horizontal")*playerHorizontalSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(direction*Time.fixedDeltaTime);
+
+        
+    }
+
+    
+}
