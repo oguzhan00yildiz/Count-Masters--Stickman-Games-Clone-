b17cb2dbd5c0eb07e13da81ad0bafd7ff05b81d8
Oguzhan Yildiz <oguzhan00yildiz@gmail.com>
2023-01-19 20:37:56 +0300

    UI Elements added

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject StartGamePanel;
+    [SerializeField] Transform Player;
+    [SerializeField] Transform EndLine;
+    [SerializeField] Image sliderimage;
+    private float scaledfloat;
+     private float i ;
+    
+    
+
+    public bool GameStarted = false;
+    
+    void Start()
+    {
+
+        PauseGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scaledfloat = (Player.position.z) / (EndLine.position.z - Player.position.z);
+       if (sliderimage.fillAmount < 1)
+       {
+    
+       
+            sliderimage.fillAmount = -scaledfloat; 
+       }
+
+       
+       Debug.Log(i);
+       Debug.Log(scaledfloat);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale =1;
+
+        GameStarted = true;
+        
+
+        StartGamePanel.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        GameStarted =false;
+    }
+
+   
+
+    
+
+}
